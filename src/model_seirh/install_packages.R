f8a14f323e36e1c14d71761e72de7c0980b80bcc
NeryC <nalbertoc132@gmail.com>
2023-07-30 22:51:48 -0400

    update

--- src/model_seirh/install_packages.R
+++ src/model_seirh/install_packages.R
@@ -1,28 +1,28 @@
-# install.packages("tidyverse", repos = "http://cran.us.r-project.org")
-# install.packages("dplyr", repos = "http://cran.us.r-project.org")
-# install.packages("rio", repos = "http://cran.us.r-project.org")
-# install.packages("rstan", repos = "http://cran.us.r-project.org")
-# install.packages("deSolve", repos = "http://cran.us.r-project.org")
-# install.packages("bayesplot", repos = "http://cran.us.r-project.org")
-# install.packages("tictoc", repos = "http://cran.us.r-project.org")
-# install.packages("modules", repos = "http://cran.us.r-project.org")
-# install.packages("roll", repos = "http://cran.us.r-project.org")
-# install.packages("ensurer", repos = "http://cran.us.r-project.org")
-# install.packages("R.utils")
-# install.packages("fpeek")
+install.packages("tidyverse", repos = "http://cran.us.r-project.org")
+install.packages("dplyr", repos = "http://cran.us.r-project.org")
+install.packages("rio", repos = "http://cran.us.r-project.org")
+install.packages("rstan", repos = "http://cran.us.r-project.org")
+install.packages("deSolve", repos = "http://cran.us.r-project.org")
+install.packages("bayesplot", repos = "http://cran.us.r-project.org")
+install.packages("tictoc", repos = "http://cran.us.r-project.org")
+install.packages("modules", repos = "http://cran.us.r-project.org")
+install.packages("roll", repos = "http://cran.us.r-project.org")
+install.packages("ensurer", repos = "http://cran.us.r-project.org")
+install.packages("R.utils")
+install.packages("fpeek")
 install.packages("glu", repos = "http://cran.us.r-project.org")
 
-# setwd(getwd())
-# root_path <- getwd()
+setwd(getwd())
+root_path <- getwd()
 
-# installed_previously <- read.csv(paste(
-#     root_path,
-#     "/src/model_seirh/installed_previously.csv",
-#     sep = ""
-# ))
+installed_previously <- read.csv(paste(
+    root_path,
+    "/src/model_seirh/installed_previously.csv",
+    sep = ""
+))
 
-# base_r <- as.data.frame(installed.packages())
+base_r <- as.data.frame(installed.packages())
 
-# to_install <- setdiff(installed_previously, base_r)
+to_install <- setdiff(installed_previously, base_r)
 
-# install.packages(to_install, repos = "http://cran.us.r-project.org")
+install.packages(to_install, repos = "http://cran.us.r-project.org")
